04dd241af90fe7991e4e58fae917e18129755743
allen <195252944@qq.com>
2018-05-08 16:48:06 +0800

    模块整理

---
+++ nongjiayouxian-manager/nongjiayouxian-web/src/main/java/org/zhouyou/nongjiayouxian/NongjiayouxianWebApplication.java
@@ -0,0 +1,12 @@
+package org.zhouyou.nongjiayouxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NongjiayouxianWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NongjiayouxianWebApplication.class, args);
+	}
+}
